ccee9ceeb59294c72806808d43cde70b181f6b28
paithiov909 <am11.44@gmail.com>
2021-11-06 04:09:04 +0900

    perf: update aozora-reader

--- R/aozora-reader.R
+++ R/aozora-reader.R
@@ -25,38 +25,30 @@ read_aozora_impl <- function() {
 
     new_file <- file.path(directory, paste0(txtname, ".txt"))
 
-    if (file.create(new_file)) {
-      outfile <- file(new_file, open = "ab", encoding = "UTF-8")
+    flag <- TRUE
 
-      flag <- TRUE
-      reg1 <- enc2utf8("^\u5e95\u672c")
-      reg2 <- enc2utf8("\u3010\u5165\u529b\u8005\u6ce8\u3011")
-      reg3 <- enc2utf8("\uff3b\uff03[^\uff3d]*\uff3d")
-      reg4 <- enc2utf8("\u300a[^\u300b]*\u300b")
-      reg5 <- enc2utf8("\uff5c")
-
-      lines <- readLines(connection, n = -1L, encoding = "CP932")
-      lines <- iconv(lines, from = "CP932", to = "UTF-8")
-      lines <- ldcc_conv_normalize(lines)
-
-      for (line in lines) {
-        if (stringr::str_detect(line, reg1)) break
-        if (stringr::str_detect(line, reg2)) break
-        if (stringr::str_detect(line, "^[-]+")) {
-          flag <- !flag
-          next
+    out <- connection %>%
+      readLines(n = -1L, encoding = "CP932") %>%
+      iconv(from = "CP932", to = "UTF-8") %>%
+      stringi::stri_replace_all_regex("([:alpha:])[`'^~:&_,'/]", "$1") %>% ## sepatated accents
+      purrr::map(function(line) {
+        if (stringi::stri_detect_regex(line, "^[-]+") ||
+          stringi::stri_detect_regex(line, "^\u5e95\u672c\uff1a") || ## teihon
+          stringi::stri_detect_regex(line, "\u3010\u5165\u529b\u8005\u6ce8\u3011")) {
+          flag <<- !flag
+          return(NA_character_)
+        } else {
+          dplyr::case_when(
+            flag ~ line %>%
+              stringi::stri_replace_all_regex("\uff3b\uff03[^\uff3d]*\uff3d", "") %>% ## comments
+              stringi::stri_replace_all_regex("\u300a[^\u300b]*\u300b", "") %>% ## ruby
+              stringi::stri_replace_all_regex("\uff5c", ""), ## tatebou
+            TRUE ~ NA_character_
+          )
         }
-        if (flag) {
-          line <- line %>%
-            stringr::str_replace("^[-]+", "") %>%
-            stringr::str_replace_all(reg3, "") %>%
-            stringr::str_replace_all(reg4, "") %>%
-            stringr::str_replace_all(reg5, "")
-          write_lines(line, outfile, append = TRUE)
-        }
-      }
-      close(outfile)
-    }
+      }) %>%
+      stringi::stri_omit_empty_na()
+    readr::write_lines(out, new_file)
     return(new_file)
   }
 }
