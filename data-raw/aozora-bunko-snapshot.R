45d0b00b0253c44a0cb3ed89e4ce3f90c96a5798
paithiov909 <am11.44@gmail.com>
2022-09-27 00:43:40 +0900

    update (20220927)

--- data-raw/aozora-bunko-snapshot.R
+++ data-raw/aozora-bunko-snapshot.R
@@ -3,7 +3,8 @@ tmp <- tempfile()
 download.file("http://www.aozora.gr.jp/index_pages/list_person_all_extended_utf8.zip", destfile = tmp)
 list_all <- unzip(tmp, exdir = tempdir())
 
-AozoraBunkoSnapshot <- readr::read_csv(list_all)
+AozoraBunkoSnapshot <- readr::read_csv(list_all, guess_max = 2000)
+# readr::problems()
 
 AozoraBunkoSnapshot <- AozoraBunkoSnapshot |>
   dplyr::mutate(作品ID = as.integer(作品ID), 人物ID = as.integer(人物ID)) |>
