5251307ff2a9f710813886748c14f8c65365b1a0
paithiov909 <am11.44@gmail.com>
2023-09-22 01:47:55 +0900

    feat: add 'read_srt'

--- R/ldccr-package.R
+++ R/ldccr-package.R
@@ -25,3 +25,22 @@ utils::globalVariables("where")
   rlang::env_bind(.pkgenv, "read_ldnws" = memoise::memoise(read_ldnws_impl()))
   rlang::env_bind(.pkgenv, "read_jrte_rte" = memoise::memoise(read_jrte_rte_impl()))
 }
+
+#' Read srt file
+#'
+#' @param path Path to srt file.
+#' @param collapse String.
+#' @return A tibble.
+#' @export
+#' @useDynLib ldccr, .registration = TRUE
+read_srt <- function(path, collapse = "\n") {
+  stopifnot(is.character(collapse))
+
+  path <- path.expand(path)
+  ret <- read_srt_impl(path, collapse = collapse)
+
+  ret$start <- stringi::stri_replace_all_regex(ret$start, pattern = ",", replacement = "\\.")
+  ret$end <- stringi::stri_replace_all_regex(ret$end, pattern = ",", replacement = "\\.")
+
+  tibble::as_tibble(ret)
+}
